c1f3649e36cdb29f832cae23ab3f9b32a780d023
jiaying.lu <jiaying.lu@emory.edu>
2020-08-27 22:25:51 -0400

    finish Bitmap, Grayscale and Sepia

--- lab0/ppmcvt.c
+++ lab0/ppmcvt.c
@@ -14,13 +14,25 @@
 PPMImage * new_ppmimage(unsigned int width, unsigned int height, unsigned int max) {
     PPMImage *image = NULL;
     image = malloc(sizeof *image);
+    if (image == NULL) {
+        fprintf(stderr, ERR_MALLOC);
+        exit(1);
+    }
     image->width = width;
     image->height = height;
     image->max = max;
     for (int i=0; i<3; i++) {
         image->pixmap[i] = malloc(height * sizeof(unsigned int *));
+        if (image->pixmap[i] == NULL) {
+            fprintf(stderr, ERR_MALLOC);
+            exit(1);
+        }
         for (int h=0; h<height; h++) {
             image->pixmap[i][h] = malloc(width * sizeof(unsigned int));
+            if (image->pixmap[i][h] == NULL) {
+                fprintf(stderr, ERR_MALLOC);
+                exit(1);
+            }
         }
     }
     return image;
@@ -29,12 +41,24 @@ PPMImage * new_ppmimage(unsigned int width, unsigned int height, unsigned int ma
 PGMImage * new_pgmimage( unsigned int width, unsigned int height, unsigned int max) {
     PGMImage *image = NULL;
     image = malloc(sizeof *image);
+    if (image == NULL) {
+        fprintf(stderr, ERR_MALLOC);
+        exit(1);
+    }
     image->width = width;
     image->height = height;
     image->max = max;
     image->pixmap = malloc(height * sizeof(unsigned int *));
+    if (image->pixmap == NULL) {
+        fprintf(stderr, ERR_MALLOC);
+        exit(1);
+    }
     for (int h=0; h<height; h++) {
         image->pixmap[h] = malloc(width * sizeof(unsigned int));
+        if (image->pixmap[h] == NULL) {
+            fprintf(stderr, ERR_MALLOC);
+            exit(1);
+        }
     }
     return image;
 }
@@ -42,11 +66,23 @@ PGMImage * new_pgmimage( unsigned int width, unsigned int height, unsigned int m
 PBMImage * new_pbmimage( unsigned int width, unsigned int height ) {
     PBMImage *image = NULL;
     image = malloc(sizeof *image);
+    if (image == NULL) {
+        fprintf(stderr, ERR_MALLOC);
+        exit(1);
+    }
     image->width = width;
     image->height = height;
     image->pixmap = malloc(height * sizeof(unsigned int *));
+    if (image->pixmap == NULL) {
+        fprintf(stderr, ERR_MALLOC);
+        exit(1);
+    }
     for (int h=0; h<height; h++) {
         image->pixmap[h] = malloc(width * sizeof(unsigned int));
+        if (image->pixmap[h] == NULL) {
+            fprintf(stderr, ERR_MALLOC);
+            exit(1);
+        }
     }
     return image;
 }
@@ -78,6 +114,47 @@ void del_pbmimage( PBMImage * image) {
     image = NULL;
 }
 
+// convertion functions
+PBMImage * convert_ppm_to_pbm(PPMImage * ppm) {
+    PBMImage * pbm = new_pbmimage(ppm->width, ppm->height);
+    for (int h=0; h<ppm->height; h++) {
+        for (int w=0; w<ppm->width; w++) {
+            pbm->pixmap[h][w] = (ppm->pixmap[0][h][w] + ppm->pixmap[1][h][w] \
+                    + ppm->pixmap[2][h][w]) / 3.0 < ppm->max / 2.0;
+        }
+    }
+    return pbm;
+}
+
+PGMImage * convert_ppm_to_pgm(PPMImage * ppm, unsigned int pgm_max) {
+    printf("[DEBUG] inside convert_ppm_to_pgm, pgm_max=%u\n", pgm_max);
+    PGMImage * pgm = new_pgmimage(ppm->width, ppm->height, pgm_max);
+    for (int h=0; h<ppm->height; h++) {
+        for (int w=0; w<ppm->width; w++) {
+            pgm->pixmap[h][w] = (ppm->pixmap[0][h][w] + ppm->pixmap[1][h][w] \
+                    + ppm->pixmap[2][h][w]) / 3.0 / ppm->max * pgm->max;
+        }
+    }
+    return pgm;
+}
+
+void sepia_transform_inplace(PPMImage * ppm) {
+    unsigned int new_r, new_g, new_b;
+    for (int h=0; h<ppm->height; h++) {
+        for (int w=0; w<ppm->width; w++) {
+            new_r = 0.393 * ppm->pixmap[0][h][w] + 0.769 * ppm->pixmap[1][h][w] \
+                    + 0.189 * ppm->pixmap[2][h][w];
+            new_g = 0.349 * ppm->pixmap[0][h][w] + 0.686 * ppm->pixmap[1][h][w] \
+                    + 0.168 * ppm->pixmap[2][h][w];
+            new_b = 0.272 * ppm->pixmap[0][h][w] + 0.534 * ppm->pixmap[1][h][w] \
+                    + 0.131 * ppm->pixmap[2][h][w];
+            ppm->pixmap[0][h][w] = min(new_r, ppm->max);
+            ppm->pixmap[1][h][w] = min(new_g, ppm->max);
+            ppm->pixmap[2][h][w] = min(new_b, ppm->max);
+        }
+    }
+}
+
 
 // argument parse
 int main (char argc, char *argv[]) {
@@ -86,9 +163,38 @@ int main (char argc, char *argv[]) {
     int opt, err = 0;
     char *in_fname = NULL;
     char *out_fname = NULL;
+    char conv = 'b';
+    bool have_conv = false;
+    long ret;
+    char * cptr;
+    PPMImage * ppm = NULL;
+    PBMImage * pbm = NULL;
+    PGMImage * pgm = NULL;
     
     while ((opt = getopt(argc, argv, "bg:i:r:smt:n:o:")) != -1) {
         switch (opt) {
+            case 'b':
+            case 's':
+                if (have_conv) {
+                    fprintf(stderr, ERR_MULTI_CONV);
+                    exit(1);
+                }
+                conv = opt;
+                have_conv = true;
+                break;
+            case 'g':
+                if (have_conv) {
+                    fprintf(stderr, ERR_MULTI_CONV);
+                    exit(1);
+                }
+                conv = 'g';
+                have_conv = true;
+                ret = strtol(optarg, &cptr, 10);
+                if (ret < 1 || ret > 65535) {
+                    fprintf(stderr, ERR_INVALID_G, optarg);
+                    exit(1);
+                }
+                break;
             case 'o':
                 out_fname = optarg;
                 break;
@@ -98,26 +204,42 @@ int main (char argc, char *argv[]) {
         }
     }
 
-    if (out_fname == NULL) out_fname = DEFAULT_OUT_FNAME;   //TODO: output to stdout
-    printf("[DEBUG] out_fname=%s\n", out_fname);
-
-    printf("[DEBUG] optind=%d, argc=%d\n", optind, argc);
-    if (optind + 1 == argc) {
-        in_fname = argv[optind];
-    } else if (optind == argc) {
-        in_fname = DEFAULT_IN_FNAME;  //TODO: read from stdin
-    } else {
-        err = 1;
-    }
-    printf("[DEBUG] in_fname=%s\n", in_fname);
-
     if (err) {
         fprintf(stderr, USAGE);
 	exit(1);
     }
 
-    PPMImage * ppm = read_ppmfile(in_fname);
-    printf("[DEBUG] ppm readed, h=%u, w=%u, maxval=%u\n", ppm->height, 
-            ppm->width, ppm->max);
+    // printf("[DEBUG] optind=%d, argc=%d\n", optind, argc);
+    if (optind + 1 == argc) {
+        in_fname = argv[optind];
+    } else {
+        fprintf(stderr, ERR_NO_IN);
+        exit(1);
+    }
+    if (out_fname == NULL) {
+        fprintf(stderr, ERR_NO_OUT);
+        exit(1);
+    }
+
+    ppm = read_ppmfile(in_fname);
+    switch (conv) {
+        case 'b':
+            pbm = convert_ppm_to_pbm(ppm);
+            write_pbmfile(pbm, out_fname);
+            del_pbmimage(pbm);
+            break;
+        case 'g':
+            pgm = convert_ppm_to_pgm(ppm, ret);
+            write_pgmfile(pgm, out_fname);
+            del_pgmimage(pgm);
+            break;
+        case 's':
+            sepia_transform_inplace(ppm);
+            write_ppmfile(ppm, out_fname);
+        case '?':
+            // should not touch this cond
+            break;
+    }
     del_ppmimage(ppm);
+    return 0;
 }
--- lab0/ppmcvt.h
+++ lab0/ppmcvt.h
@@ -6,8 +6,34 @@
 #ifndef ppmcvt_h
 #define ppmcvt_h
 
-#define DEFAULT_IN_FNAME "in.ppm"
-#define DEFAULT_OUT_FNAME "out.pbm"
 #define USAGE "Usage: ppmcvt [-bgirsmtno] [FILE]\n"
+#define ERR_NO_IN "Error: NO input file specified\n"
+#define ERR_NO_OUT "Error: NO output file specified\n"
+#define ERR_MALLOC "Error: Memory allocation failed\n"
+#define ERR_MULTI_CONV "Error: Multiple transformations specified\n"
+#define ERR_INVALID_G "Error: Invalid max grayscale pixel value: %s; must be less than 65,536\n"
+
+#include "pbm.h"
+
+typedef enum {false, true} bool;
+
+#define max(a,b)             \
+({                           \
+    __typeof__ (a) _a = (a); \
+    __typeof__ (b) _b = (b); \
+    _a > _b ? _a : _b;       \
+})
+
+#define min(a,b)             \
+({                           \
+    __typeof__ (a) _a = (a); \
+    __typeof__ (b) _b = (b); \
+    _a < _b ? _a : _b;       \
+})
+
+// convertion functions
+PBMImage * convert_ppm_to_pbm(PPMImage *);
+PGMImage * convert_ppm_to_pgm(PPMImage *, unsigned int);
+void sepia_transform_inplace(PPMImage *);
 
 #endif /* ppmcvt_h */
